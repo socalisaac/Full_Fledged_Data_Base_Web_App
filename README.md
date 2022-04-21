a494f20347961afbb64569830d7897461ed21c39
Isaac Estrada <socalisaac@yahoo.com>
2022-04-20 21:05:03 -0700

    restart

--- README.md
+++
@@ -1,3 +0,0 @@
-# SP22-CS431-07
-Group 7 Web App
-Matheo was here test
--- _objects/login.js
+++
@@ -1,8 +0,0 @@
-import { Item } from '../app/result.js';
-
-export class Login extends Item {
-
-    constructor(data) {
-        super(data);
-    }
-}
