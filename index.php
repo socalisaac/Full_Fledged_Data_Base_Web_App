9fa1b919a21b6cc400c82e9f58b09830ece35124
Gavin Gray <graygavin111997@gmail.com>
2022-04-27 18:46:46 -0700

    Color edits

--- index.php
+++ index.php
@@ -29,6 +29,7 @@ if (!preg_match('/^[\w-]+$/', $page)) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link class="page-css" id="css_<?php echo ($page); ?>" rel="stylesheet" href="css/<?php echo ($page); ?>.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400&display=swap" rel="stylesheet">
@@ -67,7 +68,7 @@ if (!preg_match('/^[\w-]+$/', $page)) {
 
                             <li class="nav-item">
 
-                                <a class="nav-link d-felx" href='login?logout=1'><?php echo ($username); ?> (Log Out)</a>
+                                <a class="nav-link d-felx" href='login?logout=1'><?php echo ($username); ?> Log Out <i class="fa-solid fa-right-from-bracket"></i></a>
                             </li>
                         <?php } ?>
 
